9784016b37306a3ef4f1311f4331544ba63dd447
Yash121l <98008765+Yash121l@users.noreply.github.com>
2024-05-15 15:09:57 +0530

    Update HomePage, Signin, and Signup components with new styling and layout
    The HomePage component now includes a updated footer with a new copyright date and creator credits. The Signin and Signup components have been updated with a new style.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -81,15 +81,13 @@ const HomePage = () => {
                 {/*Footer*/}
                 <div className="w-full pt-16 pb-6 text-sm text-center md:text-left fade-in">
                     <a className="text-gray-500 no-underline hover:no-underline" href="#">
-                        © App 2020
+                        © May 2024
                     </a>
-                    - Template by
-                    <a
+                    - Created by {' '}
+                    <span
                         className="text-gray-500 no-underline hover:no-underline"
-                        href="https://www.tailwindtoolbox.com"
-                    >
-                        TailwindToolbox.com
-                    </a>
+                    > Rachit Kumar, Subham Mahapatra, Yash Lunawat
+                    </span>
                 </div>
             </div>
         </div>
--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -49,32 +49,108 @@ const Signin = ({setIsLogin}) => {
         }
     }
         return (
-            <div>
-                <h1>Sign In</h1>
-                <input
-                    type="text"
-                    className=""
-                    name="email"
-                    placeholder="Email"
-                    value={formData.email}
-                    onChange={handleChange}
+            // <div>
+            //     <h1>Sign In</h1>
+            //     <input
+            //         type="text"
+            //         className=""
+            //         name="email"
+            //         placeholder="Email"
+            //         value={formData.email}
+            //         onChange={handleChange}
+            //     />
+            //     <br />
+            //     <input
+            //         type="password"
+            //         className=""
+            //         name="password"
+            //         placeholder="Password"
+            //         value={formData.password}
+            //         onChange={handleChange}
+            //     /> <Link to={"/forgetpass"}>forget password ? </Link>
+            //     <br />
+            //     <button type="submit" onClick={handleSubmit}>Sign In</button>
+            //     <br />
+            //     Didn't have any account <Link to="/signup">Create new!</Link>
+            //     <br />
+            //     <button type="submit" onClick={handleGoogleLogin}>Login with Google</button>
+            // </div>
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img
+                    className="mx-auto h-10 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    alt="Your Company"
                 />
-                <br />
-                <input
-                    type="password"
-                    className=""
-                    name="password"
-                    placeholder="Password"
-                    value={formData.password}
-                    onChange={handleChange}
-                /> <Link to={"/forgetpass"}>forget password ? </Link>
-                <br />
-                <button type="submit" onClick={handleSubmit}>Sign In</button>
-                <br />
-                Didn't have any account <Link to="/signup">Create new!</Link>
-                <br />
-                <button type="submit" onClick={handleGoogleLogin}>Login with Google</button>
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+                    Sign in to your account
+                </h2>
             </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6" action="#" method="POST">
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
+                                Email address
+                            </label>
+                        </div>
+
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                className="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+                                Password
+                            </label>
+                        </div>
+                        <div className="mt-2">
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                value={formData.password}
+                                onChange={handleChange}
+                                className="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            onClick={handleSubmit}
+                        >
+                            Sign in
+                        </button>
+                    </div>
+                </form>
+                <p>or</p>
+                <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    onClick={handleGoogleLogin}
+                >
+                    Login with Google
+                </button>
+                <p className="mt-10 text-center text-sm text-gray-500">
+                    Didn't have Account{' '}
+                    <Link to="/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                        Create One!
+                    </Link>
+                </p>
+            </div>
+        </div>
         )
     }
 
