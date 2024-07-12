bd9c9f05c0bcd11fc438e74229bf24305eae518c
Yash121l <yashlunawat121@gmail.com>
2024-07-12 11:39:23 +0530

    "Updated Events.tsx to add EventClouser component and modify layout with new CSS classes"

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,9 +1,13 @@
+import EventClouser from "../components/EventClouser"
 
 
 const Events = () => {
     return (
-        <div>
-            <h1>Events</h1>
+        <div className="w-screen md:w-[calc(100vw-207px)] h-[calc(100vh-56px)] flex flex-col  items-center p-4 ">
+            <div className="w-full bg-[#f6f7f9] rounded-[6px] p-3">
+                <h1 className="text-slate-700 text-3xl font-bold">Events</h1>
+            </div>
+            <EventClouser />
         </div>
     )
 }
